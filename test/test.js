4bdd79f422a0a03d3bd60478feba9a2152c61e39
Eduardo Prauze <eduardoprauze@hotmail.com>
2016-10-13 14:47:37 -0400

    added tests for calculator input rules

---
+++ test/test.js
@@ -0,0 +1,95 @@
+describe('JavaScript-Calculator', function(){
+  var input;
+  beforeEach(function (done) {
+      input = document.createElement("input");
+      input.type = "text";
+      done();
+  });
+
+  describe('add input', function(){
+    beforeEach(function (done) {
+        input.value = "2";
+        done();
+    });
+
+    it('should add value to regular input', function(){
+        exp = false;
+        addInput('5', input);
+        expect(input.value).to.equal('25');
+    });
+
+    it('should add value to exponential power input', function(){
+        exp = true;
+        addInput('5', input);
+        expect(input.value).to.equal('25');
+    });
+
+  });
+
+  describe('input rules', function(){
+
+    describe('#doubleOperator', function(){
+
+      it('should not accept double operators in sequence', function(){
+          var result = doubleOperator('+', '12+');
+          expect(result).to.be.true;
+      });
+
+      it('should accept numbers after operators', function(){
+          var result = doubleOperator('4', '12+');
+          expect(result).to.be.false;
+      });
+
+    });
+
+    describe('#initialOperator', function(){
+
+      it('should accept "-" as first input', function(){
+          var result = initialOperator('-', '');
+          expect(result).to.be.false;
+      });
+
+      it('should not accept "+" as first input', function(){
+          var result = initialOperator('+', '');
+          expect(result).to.be.true;
+      });
+
+      it('should not accept "x" as first input', function(){
+          var result = initialOperator('x', '');
+          expect(result).to.be.true;
+      });
+
+      it('should not accept "÷" as first input', function(){
+          var result = initialOperator('÷', '');
+          expect(result).to.be.true;
+      });
+
+      it('should accept numbers as first input', function(){
+          var result = initialOperator('5', '');
+          expect(result).to.be.false;
+      });
+
+    });
+
+    describe('#doubleDots', function(){
+
+      it('should accept decimal point', function(){
+          var result = doubleDots('.', '123');
+          expect(result).to.be.false;
+      });
+
+      it('should not accept double decimal point in same number', function(){
+          var result = doubleDots('.', '12.2');
+          expect(result).to.be.true;
+      });
+
+      it('should accept two numbers with decimal point', function(){
+          var result = doubleDots('.', '12.2x45');
+          expect(result).to.be.false;
+      });
+
+    });
+
+  });
+  
+});
