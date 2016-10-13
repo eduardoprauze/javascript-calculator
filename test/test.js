d00a312c4886ee197c1a73d118dfe63d869d7b61
Eduardo Prauze <eduardoprauze@hotmail.com>
2016-10-13 15:21:37 -0400

    added tests for calculator input deleting and total

--- test/test.js
+++ test/test.js
@@ -91,5 +91,29 @@ describe('JavaScript-Calculator', function(){
     });
 
   });
-  
+
+  describe('clear input', function(){
+    it('should clear the input', function(){
+        input.value = "2+6";
+        clearInput(input);
+        expect(input.value).to.equal('');
+    });
+  });
+
+  describe('delete input', function(){
+    it('should only delete the last char of input', function(){
+        input.value = "2+6";
+        deleteInput(input);
+        expect(input.value).to.equal('2+');
+    });
+  });
+
+  describe('total', function(){
+    it('should evaluate the equation and update the input', function(){
+        input.value = "2+6";
+        total(input);
+        expect(input.value).to.equal('8');
+    });
+  });
+
 });
